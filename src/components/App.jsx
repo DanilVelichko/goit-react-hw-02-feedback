42f0c7020c47a41c927be57ecccca1e8655d7477
DanilVelichko <velichko.danil@gmail.com>
2023-01-18 12:20:41 +0100

    1 variant done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,11 @@
 import { Counter } from "./Counter/Counter";
 import React from 'react';
-import { Section } from "./Section/Section";
-import Statistics from "./Statistics/Statistics";
 
 export const App = () => {
   return (<>
     
      <Counter></Counter>
    
-    
    </>
     
   );
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -11,40 +11,32 @@ export class Counter extends React.Component {
         bad: 0,
     };
 
-   positivePercentage = 0;
+    positivePercentage = 0;
+    total = 0;
     
     handleGood = (e) => {
-        this.setState(prevState => {
-            return {
-                good: prevState.good + 1,
-            }
-        });
+        this.setState(prevState => { return {good: prevState.good + 1,}});
     };
     
     handleNeutral = (e) => {
-        this.setState(prevState => {
-            return {
-                neutral: prevState.neutral + 1,
-            }
-        });  
+        this.setState(prevState => {return {neutral: prevState.neutral + 1,}});  
     };
 
     handleBad = (e) => {
-        this.setState(prevState => {
-            return {
-                bad: prevState.bad + 1,
-            }
-        });
+        this.setState(prevState => {return {bad: prevState.bad + 1,}});
     };
 
     countTotalFeedback = () => {
-        return this.state.good + this.state.neutral + this.state.bad;
+        this.total = this.state.good + this.state.neutral + this.state.bad;
+        return this.total;
     };
 
     countPositiveFeedbackPercentage = () => {
-        if (this.state.neutral + this.state.bad + this.state.good === 0) { return this.positivePercentage = '0' }
-        if (this.state.neutral + this.state.bad + this.state.good > 0) {
-            return this.positivePercentage = Math.round(((this.state.good / (this.state.neutral + this.state.bad + this.state.good))) * 100);
+        if (this.total === 0) {
+            return this.positivePercentage = '0'
+        }
+        if (this.total > 0) {
+            return this.positivePercentage = Math.round(((this.state.good / (this.total))) * 100);
         }
     };
 
@@ -56,28 +48,23 @@ export class Counter extends React.Component {
                handleGood={this.handleGood}
                handleNeutral={this.handleNeutral}
                handleBad={this.handleBad}
-            /></Section>
+                    /></Section>
                 
+                <Section title="Statistics">
                 {(this.state.good + this.state.neutral + this.state.bad > 0
                     ?
-                    <Section title="Statistics">
-                <Statistics
-               countTotalFeedback={this.countTotalFeedback}
-               countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
-               state={this.state}
-               positivePercentage ={this.positivePercentage}/></Section>       
+                    <Statistics
+                    countTotalFeedback={this.countTotalFeedback}
+                    countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}
+                    state={this.state}
+                    positivePercentage ={this.positivePercentage}/>     
                     :
-                    <Section title="Statistics">
                     <Notification
-                        message="There is no feedback"  /></Section> 
+                            message="There is no feedback" />
                 )}
-  
-           
+                    </Section> 
            </>
-           
-           
         )
-
     }
 }
 
